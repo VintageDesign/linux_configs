d462ccf63ecc7d7feb272731920328afd117d4c9
Riley <riley.mathieu@ravenind.com>
2023-08-25 08:09:19 -0400

    Add gitconfig and ignore to setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 cp bashrc ~/.bashrc
 cp vimrc ~/.vimrc
 
-# Pre commit hook fomatter
+# Pre commit hook fomatter deps
 sudo apt install -y cmake-format clang-format black
+
 git config --global init.templatedir '~/.git-templates'
+cp  gitignore ~/.gitignore
+cp  gitconfig ~/.gitconfig
 cp -r git-templates ~/.git-templates
 
