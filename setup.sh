ed62de5169b7b7665880eea0190693869f327366
Riley Mathieu <koppril7@gmail.com>
2025-10-31 06:26:26 -0400

    Add a few more tools to the apt install

--- setup.sh
+++ setup.sh
@@ -1,14 +1,20 @@
+echo "Moving Dotfiles..."
 cp bashrc ~/.bashrc
 cp vimrc ~/.vimrc
 cp tmux.conf ~/.tmux.conf
 cp fzf.bash ~/.fzf.bash
 cp -r commands ~/commands
 
-# Pre commit hook fomatter deps
-sudo apt install -y fzf tmux cmake-format clang-format black
+echo "Installing tools..."
+sudo apt install -y tmux ripgrep cmake-format clang-format black
 
+
+echo "Configuring Git..."
 git config --global init.templatedir '~/.git-templates'
 cp  gitignore ~/.gitignore
 cp  gitconfig ~/.gitconfig
 cp -r git-templates ~/.git-templates
 
+echo "Installing FZF..."
+git clone --depth 1 git@github.com:junegunn/fzf.git ~/.fzf
+~/.fzf/install
